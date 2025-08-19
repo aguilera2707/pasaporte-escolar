2333f329947195f93da2124353092927eac3434a
Alberto Aguilera <aguilera2707@gmail.com>
2025-08-19 10:46:17 -0600

    Se cambia contraseña de aplicacion

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'pasaporte-no-reply@cela.edu.mx'
-app.config['MAIL_PASSWORD'] = 'lrznarqzdskamqxm'  # ← contraseña de aplicación
+app.config['MAIL_PASSWORD'] = 'grgiuhmaoobcgeap'  # ← contraseña de aplicación
 app.config['MAIL_DEFAULT_SENDER'] = 'pasaporte-no-reply@cela.edu.mx'
 
 # Inicialización de extensiones
