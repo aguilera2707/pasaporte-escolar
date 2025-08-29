b29a74536b92412aa075862469a88747ed1b8370
Alberto Aguilera <aguilera2707@gmail.com>
2025-08-29 11:54:44 -0600

    REparando escaneo de codigos v2

--- app/extensions.py
+++ app/extensions.py
@@ -3,4 +3,4 @@ from flask_login import LoginManager
 
 mail = Mail()
 login_manager = LoginManager()
-login_manager.login_view = "login"  # aquí debe coincidir con tu ruta de login
+login_manager.login_view = "login_familia"  # aquí debe coincidir con tu ruta de login
